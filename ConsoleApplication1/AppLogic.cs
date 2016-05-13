76dd7eb09073d81d0aaa82bf94ff38b561d87387
Tai Nguyen <tainguyen@nhatvietgroup.com.vn>
2016-05-13 13:20:32 +0700

    Source codes

---
+++ ConsoleApplication1/AppLogic.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    public class AppLogic
+    {
+        public void ExecuteTask(string[] args)
+        {
+            //TODO: Add applogic here
+        }
+    }
+}
